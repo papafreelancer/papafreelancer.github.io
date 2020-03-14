ec3044ec8aa7a49fc8eea05cc20f5032909bea13
Pavel Danilov <p.danilovv@gmail.com>
2020-03-14 21:25:26 +0800

    Add Dark Mode

--- tricksCSS/scripts.js
+++ tricksCSS/scripts.js
@@ -9,3 +9,16 @@ $ ('#my_btn').hover(function(){
         left: getRandomInt(0, 700)+"px"
     });
 }, function(){ });
+
+// $('#color_btn').click(function(){
+//     $("#color_btn").text("Light Mode");
+// });
+$('.dark').click(function(){
+    var $this = $(this);
+    $this.toggleClass('.dark');
+    if($this.hasClass('.dark')){
+        $this.text('Dark Mode');			
+    } else {
+        $this.text('Light Mode');
+    }
+});
